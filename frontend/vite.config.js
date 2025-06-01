22f63797dca75aca43d99524efed884e3a5c888d
Sureshstha14 <sureshstha147@gmail.com>
2025-06-01 18:16:17 +0530

    Base Added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port :4000 }
+  server:{port :4000 },
+  base: "https://github.com/Sureshstha14/Ecommerce-.git",
 
 })
